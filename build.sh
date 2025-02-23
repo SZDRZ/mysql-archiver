f4b14c4e373fe4e23e978ba3c05bd51d98f7eb99
CrazyZhao <3203623532@qq.com>
2025-02-23 17:12:11 +0800

    update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-rm -rf bin/
-go build -o bin/
+if command -v docker &>/dev/null; then
+    echo "Docker detected. Starting build with Docker..."
+    sudo rm -rf bin/ && sudo docker run -v $PWD:/workspace golang:1.21.10 build -o bin/
+    build_status=$?
+else
+    echo "Docker command not found. Try to local build..."
+    go build -o bin/
+    build_status=$?
+fi
+
+if [ $build_status -eq 0 ]; then
+    echo "Build success!"
+else
+    echo "Build failed!"
+fi
